d9ad1332ebc431bc54fc0fe29cd1c28f411b4b9c
Nathan Pham <nathanpham.me@gmail.com>
2021-06-16 21:59:11 -0700

    tests

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 import { s } from "./s.js"
 
+const tests = [
+    {
+        value: undefined,
+        options: undefined
+    }
+].forEach((test, i) => {
+    console.log(
+        s(test.value, test.options) ? "passed" : "failed",
+        "test:",
+        i
+    )
+})
